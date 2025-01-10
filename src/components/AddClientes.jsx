96d524f0c1ed29f6e2870d70c391916942749568
schelk <isaacfschelk@outlook.com>
2025-01-10 06:53:46 -0300

    testes

--- src/components/AddClientes.jsx
+++ src/components/AddClientes.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { FaWindowClose } from "react-icons/fa";
 import { GiConfirmed } from "react-icons/gi";
+
 const AddClientes = ({ modal }) => {
   const [formData, setFormData] = useState({
     name: "",
@@ -22,36 +23,30 @@ const AddClientes = ({ modal }) => {
     <div className="form-container">
       <FaWindowClose onClick={modal} className="close" />
       <form onSubmit={handleSubmit} className="general-forms">
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-          />
-        </div>
+        <label htmlFor="name">Nome:</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+        <label htmlFor="email">E-Mail:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <label htmlFor="password">Senha:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
         <GiConfirmed type="submit" className="close" onClick={modal} />
       </form>
     </div>
